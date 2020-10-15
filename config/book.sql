d3f7b213cda1178724170bdf5457e0ac7e5072b2
fenghaojiang <457420285@qq.com>
2020-10-15 17:55:22 +0800

    Update book.sql

--- config/book.sql
+++ config/book.sql
@@ -1,14 +1,14 @@
 CREATE TABLE `douban_books` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(30) DEFAULT '' COMMENT '标题',
-  `subtitle` varchar(999) DEFAULT '' COMMENT '副标题',
+  `author` varchar(50) DEFAULT `` COMMENT '作者',
   `other` varchar(999) DEFAULT '' COMMENT '其他',
   `desc` varchar(999) DEFAULT '' COMMENT '简述',
-  `year` varchar(20) DEFAULT '' COMMENT '年份',
+  `date` varchar(20) DEFAULT '' COMMENT '日期',
   `area` varchar(20) DEFAULT '' COMMENT '地区',
   `tag` varchar(20) DEFAULT '' COMMENT '标签',
   `star` int(10) unsigned DEFAULT '0' COMMENT 'star',
   `comment` int(10) unsigned DEFAULT '0' COMMENT '评分',
   `quote` varchar(999) DEFAULT '' COMMENT '引用',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='豆瓣电影Top250';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='豆瓣读书Top250';
