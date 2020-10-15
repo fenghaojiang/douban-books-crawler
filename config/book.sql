924e27656b16d894b7329ff5d10d5c390f3b60db
fenghaojiang <457420285@qq.com>
2020-10-16 00:12:52 +0800

    Update book.sql

--- config/book.sql
+++ config/book.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `douban_book` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(999) DEFAULT '' COMMENT '标题',
+  `title` varchar(20) DEFAULT '' COMMENT '标题',
   `author` varchar(50) DEFAULT '' COMMENT '作者',
   `translator` varchar(50) DEFAULT '' COMMENT '译者',
   `press` varchar(50) DEFAULT '' COMMENT '出版社',
   `date` varchar(20) DEFAULT '' COMMENT '日期',
-  `price` varchar(50) DEFAULT '' COMMENT '定价',
+  `price` varchar(20) DEFAULT '' COMMENT '定价',
   `star` int(10) unsigned DEFAULT '0' COMMENT '星级',
   `comment` int(10) unsigned DEFAULT '0' COMMENT '评分',
   `quote` varchar(999) DEFAULT '' COMMENT '引用',
